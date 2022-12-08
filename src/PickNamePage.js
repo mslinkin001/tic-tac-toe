2a4ed417850d589eb4cd237374fb197bf57abc42
milad sohrabi <milad.bs@gmail.com>
2022-12-08 14:48:37 +0330

    "control the color of select inputs"

--- src/PickNamePage.js
+++ src/PickNamePage.js
@@ -1,9 +1,28 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import "./PickNamePage.css";
 
 function PickNamePage(props) {
+  const [isSelectClassChange, setSelectClassChange] = useState(false);
+  const [isSelectClassChange2, setSelectClassChange2] = useState(false);
+
   const enteredName1 = useRef();
   const enteredName2 = useRef();
+  const selectChangeHandler = (e) => {
+    const targetValue = e.target.value;
+    if (targetValue === "O") {
+      setSelectClassChange(true);
+    } else {
+      if (isSelectClassChange) setSelectClassChange(false);
+    }
+  };
+  const selectChangeHandler2 = (e) => {
+    const targetValue = e.target.value;
+    if (targetValue === "O") {
+      setSelectClassChange2(true);
+    } else {
+      if (isSelectClassChange2) setSelectClassChange2(false);
+    }
+  };
 
   const clickHandler = (e) => {
     e.preventDefault();
@@ -20,17 +39,23 @@ function PickNamePage(props) {
           <label>Choose Symbol:</label>
 
           <input type="text" ref={enteredName1} />
-          <select>
-            <option>X</option>
-            <option>O</option>
+          <select
+            style={isSelectClassChange ? { color: "red" } : {}}
+            onChange={selectChangeHandler}
+          >
+            <option className="inner-X">X</option>
+            <option className="inner-O">O</option>
           </select>
           <label>Player #2 Name:</label>
           <label>Choose Symbol:</label>
 
           <input type="text" ref={enteredName2} />
-          <select>
-            <option>X</option>
-            <option>O</option>
+          <select
+            style={isSelectClassChange2 ? { color: "red" } : {}}
+            onChange={selectChangeHandler2}
+          >
+            <option className="inner-X">X</option>
+            <option className="inner-O">O</option>
           </select>
         </div>
 
